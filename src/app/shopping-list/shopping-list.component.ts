583b5ed8a888db61329530032bdeee4b358863c9
kemenydani <kemenydani93@gmail.com>
2018-10-24 16:41:31 +0200

    progress

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
@@ -6,17 +6,12 @@ import {Ingredient} from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent {
 
   ingredients: Ingredient[] = [
       new Ingredient('Salt', 10),
       new Ingredient('Pepper', 20)
   ];
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   onIngredientAdded(ingredient: Ingredient){
       this.ingredients.push(ingredient);
