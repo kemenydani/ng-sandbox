0b483bc64a1c4ba2aa7eaa2e2ef22f2b3b9812e5
kemenydani <kemenydani93@gmail.com>
2018-10-24 16:40:30 +0200

    s6: Components & Databinding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  //event bus. Output() makes it visible for the parent component
+  @Output() featureSelected = new EventEmitter<string>();
 
-  ngOnInit() {
+  onSelect(feature: string){
+    // emit with event bus
+    this.featureSelected.emit(feature);
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from './recipe.model';
 
 @Component({
@@ -8,6 +8,8 @@ import {Recipe} from './recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
         new Recipe('Bread', 'Soft bread', 'https://www.browneyedbaker.com/wp-content/uploads/2016/05/white-bread-51-550.jpg'),
         new Recipe('Bread2', 'Soft bread2', 'https://www.browneyedbaker.com/wp-content/uploads/2016/05/white-bread-51-550.jpg')
@@ -16,6 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
